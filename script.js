b3ff0c6078c9d8c2ad922d3d9fba2abf6837f284
Dima <dima.tsymbal12345@gmail.com>
2021-11-29 14:45:15 +0200

    validation add

--- script.js
+++ script.js
@@ -1,69 +1,79 @@
-const main = document.querySelector('.main')
-const input = document.querySelector('.todo-input')
-const todoList = document.querySelector('.todo-list')
+const main = document.querySelector(".main");
+const input = document.querySelector(".todo-input");
+const todoList = document.querySelector(".todo-list");
+const form = document.querySelector(".todo-form");
 
-main.addEventListener('click', (e) => {
-    
-    let target = e.target
+main.addEventListener("click", (e) => {
+  e.preventDefault();
+  let target = e.target;
 
-    if (target.type === 'submit') {
-        e.preventDefault()
-        if(input.value.length === 0) {
-            input.value = 'Empty'
-        } else {
-            createTodoItem(input, todoList)
-        }
+  const regEx = /^[a-zA-Z0-9 ]{2,15}$/;
+
+  if (target.type === "submit") {
+    e.preventDefault();
+    if (valid(input, regEx)) {
+      input.classList.toggle("valid");
+
+      createTodoItem(input, todoList);
+    } else if (inValid(input, regEx)) {
+      props.classList.toggle("invalid");
     }
-})
+  }
+});
+
+const valid = (props, reg) => {
+  if (reg.test(props.value)) {
+    return true;
+  }
+};
+
+const inValid = (props, reg) => {
+  if (!reg.test(props.value)) {
+    return false;
+  }
+};
 
 const createTodoItem = (input, list) => {
+  const todoDiv = document.createElement("div");
+  todoDiv.classList = "todo";
 
-    const todoDiv = document.createElement('div')
-    todoDiv.classList = 'todo'
+  const todoItem = document.createElement("li");
+  todoItem.innerText = input.value;
+  todoItem.classList = "todo-item";
+  todoDiv.append(todoItem);
 
-    const todoItem = document.createElement('li')
-    todoItem.innerText = input.value
-    todoItem.classList = 'todo-item'
-    todoDiv.append(todoItem)
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "&#9745;";
+  completedButton.classList = "completed-button";
+  todoDiv.append(completedButton);
 
-    const completedButton = document.createElement('button')
-    completedButton.innerHTML = '&#9745;'
-    completedButton.classList = 'completed-button'
-    todoDiv.append(completedButton)
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = "&#10006;";
+  trashButton.classList = "trash-button";
+  todoDiv.append(trashButton);
 
-    const trashButton = document.createElement('button')
-    trashButton.innerHTML = '&#10006;'
-    trashButton.classList = 'trash-button'
-    todoDiv.append(trashButton)
+  list.append(todoDiv);
+  input.value = "";
+};
 
-    list.append(todoDiv)
-    input.value = ''
-}
+todoList.addEventListener("click", (e) => {
+  let target = e.target;
 
-todoList.addEventListener('click', (e) => {
-    let target = e.target
-    
-
-    if(target.classList[0] === 'completed-button') {
-        setStateTodos(target)
-    } else if (target.classList[0] === 'trash-button') {
-        trashTodo(target)
-    }
-    
-})
+  if (target.classList[0] === "completed-button") {
+    setStateTodos(target);
+  } else if (target.classList[0] === "trash-button") {
+    trashTodo(target);
+  }
+});
 
 const setStateTodos = (target) => {
-
-    const parent = target.parentElement
-    if(parent) {
-        parent.classList.toggle('green')
-    } 
-    else if(parent.classList = 'green') {
-        parent.classList.toggle('green')
-    }
-}
+  const parent = target.parentElement;
+  if (parent) {
+    parent.classList.toggle("green");
+  }
+};
 
 const trashTodo = (target) => {
-    const parent = target.parentElement
-    parent.remove()
-}
+  const parent = target.parentElement;
+  parent.remove();
+};
